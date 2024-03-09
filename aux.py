14bfda5d636777c7dbafd55424cf30a889cd5925
wb <wub@zhejianglab.com>
2024-03-09 23:02:40 +0800

    update to use IMbind for object

--- aux.py
+++ aux.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 from skimage import morphology
 from onevision import morphology as ovmorph
@@ -5,22 +6,22 @@ from onevision import morph_proc
 import proc_data
 from wbtool.show import wshow
 
-def isolate_filter_object(id_map, segtracker_args):
-    # isolate, then reset new generated ids
-    idmax = id_map.max()
-    id_map = ovmorph.isolate_object(id_map, True)
-    id_map = ovmorph.filter_object_size(id_map, segtracker_args['min_obj_area_ratio'], segtracker_args['max_obj_area_ratio'])
+
+def isolate_filter_object(imobj, segtracker_args):
+    # isolate, then filter small object, with reset new generated ids
+    # input: IMbind obj
+    idmax = max(imobj.get_ids())
+    imobj = ovmorph.isolate_object(imobj, True)
+    imobj = ovmorph.filter_object_size(imobj, segtracker_args['min_obj_area_ratio'], segtracker_args['max_obj_area_ratio'])
     # 对于新增加物体，将ID重整成连续分布
-    ids = morph_proc.get_obj_id(id_map)
-    if ids.max() > idmax:  #若确实发生了id数量增加
+    ids = np.array(imobj.get_ids())
+    if max(ids) > idmax:  #若确实发生了id数量增加
         id_groups = [ids[ids<=idmax], ids[ids>idmax]]
-        if ids.max() > (idmax + len(id_groups[1])):  #若确实出现不连续分布的情况 (这里要用idmax而非id_groups[0].max()，因为id_groups[0]中最后的obj可能也被删了，不能因此占用原来的id)
-            new_id = list(id_groups[0]) + list(range(idmax+1, idmax+len(id_groups[1])+1))
-            id_map = morph_proc.id_reset(id_map, ids=new_id)
-            # temp = morph_proc.split_map(id_map, id_groups)
-            # temp[1] = morph_proc.id_reset(temp[1], base=idmax+1)
-            # id_map = temp[0] + temp[1]
-    return id_map
+        if max(ids) > (idmax + len(id_groups[1])):  #若确实出现不连续分布的情况 (这里要用idmax而非id_groups[0].max()，因为id_groups[0]中最后的obj可能也被删了，不能因此占用原来的id)
+            for it in id_groups[1]:
+                idmax += 1
+                imobj.mod_id(it, idmax)
+    return imobj
 
 def do_segment(SA, segtracker_args, vid, ids, coords):
     """do segmentation of cells for each frame"""
@@ -34,15 +35,16 @@ def do_segment(SA, segtracker_args, vid, ids, coords):
         ms = SA.infer(frame, point_prompt)
         I = proc_data.filter_mask_by_size(ms, 0, segtracker_args['max_obj_area_ratio']) #segtracker_args['min_obj_area_ratio']
         ms = ms[I]
-        idt = ids[frame_idx][I]
-        ms = morph_proc.masks_to_map(ms, ids=idt)
+        frame_idt = ids[frame_idx][I]
+        # ms = morph_proc.masks_to_map(ms, ids=frame_idt)
+        ms = ovmorph.IMbind(ms, 'masks', ids=frame_idt)
         pred_mask = isolate_filter_object(ms, segtracker_args)        
 
         seg_list.append(pred_mask)
         print("segment for frame {}".format(frame_idx), end='\r')
 
-    # 对于第一个segment map，将ID连续分布，因为其作为tracking的起点。
-    seg_list[0] = morph_proc.id_reset(seg_list[0])
+    # 对于第一个segment map，若不遵循ids赋值，则可将ID连续分布，因为其作为tracking的起点。
+    # seg_list[0] = morph_proc.id_reset(seg_list[0])
     print('\n-segmentation done')
     return seg_list
 
@@ -64,6 +66,59 @@ def find_new_objs(param, track_mask, seg_mask, curr_idx):
     return seg_obj_mask
 
 def merge_st(param, track_mask, seg_mask):
+    """
+    track_mask: IMbind obj
+    seg_mask: same type
+    """
+    seg_obj_ids = seg_mask.get_ids()
+    seg_obj_area = ovmorph.count_obj_area(seg_mask)
+
+    track_obj_ids = track_mask.get_ids()
+    track_obj_area = ovmorph.count_obj_area(track_mask)
+    
+    new_track_mask = copy.deepcopy(track_mask)
+    obj_num = max(track_obj_ids)
+    for nid in seg_obj_ids:  #通过idx 定位每个新的物体
+        # new object IOU with all tracked obj
+        intersect_area = np.array([np.sum(seg_mask[nid] & track_mask[it]) for it in track_obj_ids])
+        union_area = np.array([np.sum(seg_mask[nid] | track_mask[it]) for it in track_obj_ids])
+        iou = intersect_area / union_area #
+        iou_track = intersect_area / np.array(list(track_obj_area.values()))
+        # if the overlap exceed percentage of both objects, new segment object will replace that old track object (keep track id), 
+        # the old object's non overlapping part will be erased; otherwise, new object will obtain a new id, and cover the overlap part.
+        if iou_track.max() > param['new_replace_iou_min']:
+            # replace this track object with seg object mask (but keep track ids)
+            idx = np.argmax(iou_track)
+            tid = track_obj_ids[idx] #找重叠度最大的来替换
+            # 还需加一个条件，若seg的比track的大太多，还是维持track的分割。
+            if seg_obj_area[nid] <= track_obj_area[tid]*1.2:
+                new_track_mask[tid] = seg_mask[nid]
+                
+                # update obj area
+                track_obj_area[tid] = seg_obj_area[nid]
+            else:
+                pass
+        else:
+            new_track_mask.append(seg_mask[nid]) #对应new id：obj_num + 1
+    # 因为部分像素替代后，可能出现一个物体中间被断开变成若干个的情况，这里要重赋值id变成多个物体（只针对cell的应用场景）。
+    new_track_mask = ovmorph.isolate_object(new_track_mask)
+    # * 但是这里的ID对应关系改变，有需要的场景要进一步处理。
+    
+    # filter small object
+    new_track_mask = ovmorph.filter_object_size(new_track_mask, param['min_obj_area_ratio'], 1)
+
+    # filter heavily eroded objects - those shrink a lot after - only for old object in tracking.
+    temp = ovmorph.count_obj_area(new_track_mask, track_obj_ids)
+    post_obj_area = np.array([temp[it] for it in track_obj_ids])
+    ppr = post_obj_area / np.array(list(track_obj_area.values()))
+    I = ppr < param['remain_remove_max']
+    for id in range(len(track_obj_ids)):
+        if I[id]:
+            del new_track_mask[track_obj_ids[id]]
+
+    return morph_proc.imbind_to_map(new_track_mask)
+
+def merge_st_mapv(param, track_mask, seg_mask):
     """
     track_mask: a 2D id map
     seg_mask: same type
@@ -119,10 +174,3 @@ def merge_st(param, track_mask, seg_mask):
             new_track_mask[new_track_mask==obj_ids[id]] = 0
 
     return new_track_mask
-
-# def do_st_merge(segtracker_args, track_mask, seg_mask):
-#     track_mask = isolate_filter_object(track_mask, segtracker_args)
-    
-#     # merge tracking and new segment maps.
-#     pred_mask = merge_st(segtracker_args, track_mask, seg_mask)
-#     return pred_mask
